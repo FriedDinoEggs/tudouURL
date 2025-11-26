3a69271faf47bcc3704d20f8bc34d2aa208949b0
BillyShen <happy860925@gmail.com>
2025-11-26 11:12:25 +0800

    feat: Register ShortUrls model in admin.

--- apps/shorturl/admin.py
+++ apps/shorturl/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ShortUrls
+
 # Register your models here.
+
+
+@admin.register(ShortUrls)
+class ShortUrlsAdmin(admin.ModelAdmin):
+    list_display = ['original_url', 'create_at', 'expires_at', 'is_active', 'clicks_count']
