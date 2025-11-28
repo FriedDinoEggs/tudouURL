450ea69a0da6a4d29d8c37fbc7c34ceba4882220
BillyShen <happy860925@gmail.com>
2025-11-28 13:50:42 +0800

    feat: feat: Add try-catch for ValueError handling

--- apps/shorturl/views.py
+++ apps/shorturl/views.py
@@ -1,4 +1,5 @@
 from django.db.models import F
+from django.http import HttpResponseNotFound
 from django.shortcuts import get_object_or_404, redirect
 from rest_framework import mixins, status, viewsets
 from rest_framework.response import Response
@@ -34,22 +35,26 @@ class ShortUrlsViewSet(
 
 
 def redirectShortCode(request, short_code):
-    original_id = ShortUrlService.decode(short_code)
+    try:
+        original_id = ShortUrlService.decode(short_code)
 
-    short_url_instance = get_object_or_404(ShortUrls, pk=original_id, is_active=True)
-    short_url_instance.clicks_count = F('clicks_count') + 1
-    short_url_instance.save(update_fields=['clicks_count'])
+        short_url_instance = get_object_or_404(ShortUrls, pk=original_id, is_active=True)
+        short_url_instance.clicks_count = F('clicks_count') + 1
+        short_url_instance.save(update_fields=['clicks_count'])
 
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    log_ip_address = x_forwarded_for if x_forwarded_for else request.META.get('REMOTE_ADDR')
-    log_user_agent = request.META.get('HTTP_USER_AGENT', '')
-    log_referer = request.META.get('HTTP_REFERER')
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        log_ip_address = x_forwarded_for if x_forwarded_for else request.META.get('REMOTE_ADDR')
+        log_user_agent = request.META.get('HTTP_USER_AGENT', '')
+        log_referer = request.META.get('HTTP_REFERER')
 
-    store_log.delay(
-        short_url_id=short_url_instance.id,
-        ip_address=log_ip_address,
-        user_agent=log_user_agent,
-        referer=log_referer,
-    )
+        store_log.delay(
+            short_url_id=short_url_instance.id,
+            ip_address=log_ip_address,
+            user_agent=log_user_agent,
+            referer=log_referer,
+        )
 
-    return redirect(short_url_instance.original_url)
+        return redirect(short_url_instance.original_url)
+
+    except ValueError:
+        return HttpResponseNotFound('The requested short URL contains invalid characters.')
